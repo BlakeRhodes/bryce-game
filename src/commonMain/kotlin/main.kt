f7e4af4081d71ddb6378642c403c781f645bfb09
Blake <blakewrhodes@gmail.com>
2022-05-11 00:32:19 -0500

    this is ugly, but slightly more fun

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,9 +1,12 @@
+import com.soywiz.klock.Frequency
 import com.soywiz.korev.Key
 import com.soywiz.korge.Korge
 import com.soywiz.korge.view.*
 import com.soywiz.korim.color.Colors
 import com.soywiz.korim.format.readBitmap
 import com.soywiz.korio.file.std.resourcesVfs
+import com.soywiz.korma.geom.Rectangle
+import com.soywiz.korma.geom.vector.rect
 
 suspend fun main() = Korge(
     width = 800,
@@ -14,53 +17,65 @@ suspend fun main() = Korge(
     createBackground()
 
     val directions = text(
-        text = "WASD to move, SPACEBAR to thrill",
+        text = "WASD to move, SPACEBAR to thrill, Enter to Kill",
         textSize = 32.0,
     ).xy(100, 125)
+    val score = text(text = "Score: $difficulty", textSize = 32.0).xy(650, 500)
+    val message = text("",textSize = 32.0,).xy(100, 125)
 
-    val (walkAnimation, standAnimation, spinAnimation) = createAnimations()
+    val spriteMap = resourcesVfs["bryce-sprite-sheet.png"].readBitmap()
+    val height = 200
+    val width = 88
+    val lazers = mutableListOf<SolidRect>()
+    val villains = mutableListOf<SolidRect>()
 
-    val bryce = sprite(standAnimation).scale(.5).xy(400, 200)
-    bryce.playAnimationLooped(standAnimation)
+    val walkAnimation = SpriteAnimation(
+        spriteMap = spriteMap,
+        spriteWidth = width,
+        spriteHeight = height,
+        columns = 2,
+        marginTop = height,
+    )
+    val standAnimation = SpriteAnimation(
+        spriteMap = spriteMap,
+        spriteWidth = width,
+        spriteHeight = height,
+        columns = 1,
+    )
+    val spinAnimation = SpriteAnimation(
+        spriteMap = spriteMap,
+        spriteWidth = width,
+        spriteHeight = height,
+        columns = 2,
+    )
+
+    val attackAnimation = SpriteAnimation(
+        spriteMap = spriteMap,
+        spriteWidth = width,
+        spriteHeight = height,
+        columns = 2,
+        marginTop = 400,
+    )
+
+    val bryce = sprite(standAnimation).xy(100, 200)
 
     addUpdater {
         checkToRemoveDirections(directions)
-        handleKeyPress(bryce, walkAnimation, spinAnimation)
+        handleKeyPress(bryce, walkAnimation, spinAnimation, attackAnimation, lazers, villains, score, message)
     }
 }
 
+var shots = 5
+var difficulty = 0
+var alive = true
+
 private fun Stage.checkToRemoveDirections(directions: Text) {
     if(pressed(Key.D, Key.A, Key.S, Key.W, Key.SPACE)) {
-        directions.removeFromParent()
+        directions.text = ""
     }
 }
 
 private suspend fun Stage.createBackground() {
     val backgroundBitmap = resourcesVfs["background.png"].readBitmap()
     image(backgroundBitmap).xy(0, -200).scale(.80)
-}
-
-private suspend fun createAnimations(): Triple<SpriteAnimation, SpriteAnimation, SpriteAnimation> {
-    val spriteMap = resourcesVfs["bryce-sprite.png"].readBitmap()
-    val spinMap = resourcesVfs["bryce-spin.png"].readBitmap()
-
-    val walkAnimation = SpriteAnimation(
-        spriteMap = spriteMap,
-        spriteWidth = 167,
-        spriteHeight = 378,
-        columns = 2,
-    )
-    val standAnimation = SpriteAnimation(
-        spriteMap = spriteMap,
-        spriteWidth = 167,
-        spriteHeight = 378,
-        columns = 1,
-    )
-    val spinAnimation = SpriteAnimation(
-        spriteMap = spinMap,
-        spriteWidth = 167,
-        spriteHeight = 378,
-        columns = 2
-    )
-    return Triple(walkAnimation, standAnimation, spinAnimation)
 }
--- src/commonMain/kotlin/movement.kt
+++ src/commonMain/kotlin/movement.kt
@@ -1,10 +1,24 @@
 import com.soywiz.klock.TimeSpan
 import com.soywiz.korev.Key
-import com.soywiz.korge.view.Sprite
-import com.soywiz.korge.view.SpriteAnimation
-import com.soywiz.korge.view.Stage
+import com.soywiz.korge.view.*
+import com.soywiz.korim.color.Colors
+import kotlin.random.Random
 
-fun Stage.handleKeyPress(sprite: Sprite, walkAnimation: SpriteAnimation, spinAnimation: SpriteAnimation) {
+fun Stage.handleKeyPress(
+    sprite: Sprite,
+    walkAnimation: SpriteAnimation,
+    spinAnimation: SpriteAnimation,
+    attackAnimation: SpriteAnimation,
+    lazers: MutableList<SolidRect>,
+    villians: MutableList<SolidRect>,
+    score: Text,
+    message: Text,
+) {
+    val deathMessage =  text(
+        text = "",
+        color = Colors.RED,
+        textSize = 32.0,
+    ).xy(75, 125)
     val move: (Int, Boolean) -> Unit = { distance, xAxis ->
         if(xAxis) sprite.x += distance else sprite.y += distance
         sprite.playAnimation(spriteAnimation = walkAnimation, spriteDisplayTime = TimeSpan(120.0))
@@ -12,22 +26,81 @@ fun Stage.handleKeyPress(sprite: Sprite, walkAnimation: SpriteAnimation, spinAni
     }
 
     val thrill: () -> Unit = {
-        sprite.playAnimation(spriteAnimation = spinAnimation, spriteDisplayTime = TimeSpan(240.0))
+        message.text = ""
+        sprite.playAnimation(
+            spriteAnimation = spinAnimation,
+            spriteDisplayTime = TimeSpan(240.0),
+        )
+
+        if(shots <= 5*(difficulty)) shots++
     }
 
-    Key.values().filter { pressed(it) }.forEach {
-        when(it) {
-            Key.D -> move(5, true)
-            Key.A -> move(-5, true)
-            Key.S -> move(5, false)
-            Key.W -> move(-5, false)
-            Key.SPACE -> thrill()
-            else -> Unit
+    val attack: () -> Unit = {
+        if(shots > 0) {
+            sprite.playAnimation(spriteAnimation = attackAnimation, spriteDisplayTime = TimeSpan(240.0))
+
+            lazers.add(
+                solidRect(width = 20, height = 6, color = Colors.RED).xy(
+                    sprite.x + sprite.width,
+                    sprite.y + sprite.height / 2
+                )
+            )
+            shots--
+        } else message.text = "You have to Thrill, to Kill"
+    }
+
+    if(alive) {
+        if(input.keys.pressing(Key.SPACE)) thrill()
+        if(input.keys.justPressed(Key.ENTER)) attack()
+        if(input.keys.pressing(Key.D)) move(5, true)
+        if(input.keys.pressing(Key.A)) move(-5, true)
+        if(input.keys.pressing(Key.S)) move(5, false)
+        if(input.keys.pressing(Key.W)) move(-5, false)
+    }
+
+    lazers.forEach {
+        it.x += 20
+        if(it.x > 800) it.removeFromParent()
+    }
+
+    lazers.removeAll { it.parent == null }
+
+
+    if(villians.size < (difficulty+2)) {
+        difficulty++
+        score.text = "Score: $difficulty"
+        for(i in 1..Random.nextInt(5)*difficulty) {
+            val villian = solidRect(width = 30, height = 30, color = Colors.BLUE).xy(
+                Random.nextInt(from = 700, until = 800), Random.nextInt(from = 100, until = 300)
+            )
+            villians.add(villian)
         }
     }
+
+    villians.forEach {
+        it.color = Colors.colorsByName.values.random()
+        if(it.collidesWith(lazers)) {
+            it.removeFromParent()
+        } else {
+            it.x += Random.nextInt(from = -10, until = 5)
+            it.y += Random.nextInt(from = -5, until = 5)
+            reposition(it)
+        }
+
+        if(it.collidesWith(sprite)) {
+           deathMessage.text = "Nice, now Bryce is dead.\n Are you happy with yourself?"
+            sprite.removeFromParent()
+            alive = false
+        }
+
+    }
+
+    villians.removeAll { it.parent == null }
+
+
 }
 
-fun reposition(sprite: Sprite) {
+fun reposition(sprite: View) {
     val height = sprite.height * sprite.scale
     val width = sprite.width * sprite.scale
 
